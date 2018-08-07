fec3a9cf0f72bfa77f16a6eb46313796f6799ce4
romanzes <bra@ibrae.ac.ru>
2018-08-07 14:20:25 +0300

    Command line input

--- process_messenger.py
+++ process_messenger.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import os
 import shlex
+import socket
+from pprint import pprint
 from subprocess import Popen
 
 
@@ -9,11 +11,12 @@ class ProcessMessenger:
     def __init__(self, processes, mailing_list, n_log_lines, is_full_log, ssh):
         """
         Class for messaging processes states
-        :param dict processes: {'pid': {'status': 1 (running) or 0 (done) or None, 'abs_log_path': 'path' or None, 'name': 'process name' or None}}
+        :param dict processes: {'pid': {'status': 1 (running) or 0 (done) or None, 'abs_log_path': 'path' or None,
+        'name': 'process name' or None}}
         :param list mailing_list: emails ['local-part@domain', ...]
         :param int n_log_lines: number of lines from log end to email
         :param bool is_full_log: If True attach full log to email (Warning: Log may be too large!)
-        :param str or None ssh: username@host
+        :param str ssh: username@host or host or None
         """
         self.processes = processes
         self.mailing_list = mailing_list
@@ -51,7 +54,8 @@ class ProcessMessenger:
         print('Preparing')
         abs_log_path = self.processes[pid].get('abs_log_path', None)
         name = self.processes[pid].get('name', None)
-        subject = 'Done_Name:{}_PID:{}'.format(name, pid)
+        hostname = socket.gethostname()
+        subject = 'Done_Host:{}_Name:{}_PID:{}'.format(hostname, name, pid)
         print('Subject: {}'.format(subject))
         body = ''
         attachment = None
@@ -100,29 +104,52 @@ class ProcessMessenger:
 
 
 def main():
-    # Get arguments
+    print('Arguments')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help='oath to config json file', default='process_messenger_config.json')
+    parser.add_argument('-c', '--config', help='path to config json file', default='process_messenger_config.json')
+    parser.add_argument('-i', '--input', help='path to input json file')
+    parser.add_argument('-p', '--pids', help='processes ids', nargs='+')
+    parser.add_argument('-l', '--logs', help='processes logs paths', nargs='+')
+    parser.add_argument('-n', '--names', help='processes names', nargs='+')
     args = parser.parse_args()
     print(args)
     # Get config
     # "input": "path to input json file",
-    # "ssh": "username@host",
+    # "ssh": "username@host" or "host" or null,
     # "mailing_list": ["local-part@domain", ...],
     # "n_log_lines": int,
     # "full_log": bool
     with open(args.config) as f:
         config_args = json.load(f)
-    # Get input
+    print('Configs')
+    pprint(config_args)
+    # Get input from input file or from command line
     # "pid": {
     #   "abs_log_path": "abs path to process log file" or null,
-    #   "status": 1 or null,
+    #   "status": 1 or 0 or null,
     #   "name": "process name" or null
     # }
-    with open(config_args['input']) as f:
-        processes = json.load(f)
-    print(processes)
-    # Init ProcessMessenger
+    if args.input is None:
+        input_file_path = config_args['input']
+    else:  # if -i key
+        input_file_path = args.input
+    print('Input file: {}'.format(input_file_path))
+    if args.pids is None:
+        with open(input_file_path) as f:
+            processes = json.load(f)
+    else:  # if -p key
+        processes = dict()
+        for i, pid in enumerate(args.pids):
+            processes[pid] = dict()
+            if args.logs is not None:  # if -l key
+                abs_log_path = os.path.expanduser(args.logs[i]) if i < len(args.logs) else None
+                processes[pid]['abs_log_path'] = abs_log_path
+            if args.names is not None:  # if -n key
+                name = args.names[i] if i < len(args.names) else None
+                processes[pid]['name'] = name
+    print('Processes')
+    pprint(processes)
+    print('Initializing ProcessMessenger')
     pm = ProcessMessenger(
         processes, config_args['mailing_list'], config_args['n_log_lines'],
         config_args['full_log'], config_args['ssh'])
@@ -132,7 +159,7 @@ def main():
         is_changed = pm.check_processes()  # Update processes statuses
         if is_changed:
             print('Updating input file')
-            with open(config_args['input'], 'w') as f:
+            with open(input_file_path, 'w') as f:
                 json.dump(pm.processes, f, indent=2)
             print('Done updating input file')
         all_done = True

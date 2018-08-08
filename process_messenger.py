7a6a7b6e6a91fcbb0cc12dc44e82f5746c2aee6a
romanzes <bra@ibrae.ac.ru>
2018-08-08 18:52:09 +0300

    Path checking

--- process_messenger.py
+++ process_messenger.py
@@ -5,13 +5,14 @@ import shlex
 import socket
 from pprint import pprint
 from subprocess import Popen
+import sys
 
 
 class ProcessMessenger:
     def __init__(self, processes, mailing_list, n_log_lines, is_full_log, ssh, sender):
         """
         Class for messaging processes states
-        :param dict processes: {'pid': {'status': 1 (running) or 0 (done) or None, 'abs_log_path': 'path' or None,
+        :param dict processes: {'pid': {'status': 1 (running) or 0 (done) or None, 'log_path': 'path' or None,
         'name': 'process name' or None}}
         :param list mailing_list: emails ['local-part@domain', ...]
         :param int n_log_lines: number of lines from log end to email
@@ -57,32 +58,29 @@ class ProcessMessenger:
     def send_email(self, pid):
         print('Mailing')
         print('Preparing')
-        abs_log_path = self.processes[pid].get('abs_log_path', None)
+        log_path = self.processes[pid].get('log_path', None)
         name = self.processes[pid].get('name', None)
         hostname = socket.gethostname()
-        subject = 'Done_Host:{0}_Name:{1}_PID:{2}'.format(hostname, name, pid)
+        subject = 'Done_host:{0}_name:{1}_pid:{2}'.format(hostname, name, pid)
         print('Subject: {0}'.format(subject))
-        body = str()
         attachment = None
         temp_log_path = None
-        if abs_log_path is not None:
-            if os.path.isfile(abs_log_path):
-                body = 'abs_log_path:{0}'.format(abs_log_path)
-                if not self.is_full_log:
-                    temp_log_path = os.path.abspath('process_messenger_temp_log_{0}.txt'.format(pid))
-                    lines = list()
-                    with open(abs_log_path) as f:
-                        for i, line in enumerate(reversed(f.readlines())):
-                            if i < self.n_log_lines:
-                                lines.append(line)
-                    temp_log = str().join(lines)
-                    with open(temp_log_path, 'w+') as af:
-                        af.write(temp_log)
-                    attachment = temp_log_path
-                else:
-                    attachment = abs_log_path
+        body = str()
+        if log_path is not None:
+            body = 'log_path:{0}'.format(log_path)
+            if not self.is_full_log:
+                temp_log_path = os.path.abspath('process_messenger_temp_log_{0}.txt'.format(pid))
+                lines = list()
+                with open(log_path) as f:
+                    for i, line in enumerate(reversed(f.readlines())):
+                        if i < self.n_log_lines:
+                            lines.append(line)
+                temp_log = str().join(lines)
+                with open(temp_log_path, 'w+') as af:
+                    af.write(temp_log)
+                attachment = temp_log_path
             else:
-                print("Warning! {0} log file doesn't exist".format(abs_log_path))
+                attachment = log_path
         print('Body: {0}'.format(body))
         print('Attachment: {0}'.format(attachment))
         print('Sending')
@@ -103,67 +101,108 @@ class ProcessMessenger:
             # print(split_command)
             process = Popen(split_command)
             process.wait()
-        if temp_log_path:
+        if temp_log_path is not None:
             os.remove(temp_log_path)
         print('Done mailing')
 
 
+def check_file(path):
+    """
+    Check path on the existing file in the order:
+    0. If file at path
+    1. Else if file at relative to current working directory path
+    2. Else if file at relative to running script directory path
+    3. Else if file at relative to running script directory path with eliminating all symbolics links (real)
+    -1. Else no file
+    :param str path:
+    :return dict: {'type': int, 'path': str}
+    """
+    # Expand path
+    path_expand_vars = os.path.expandvars(path)
+    path_expand_vars_user = os.path.expanduser(path_expand_vars)
+    # Get directories
+    wd_path = os.getcwd()
+    script_dir_path = os.path.dirname(os.path.abspath(__file__))
+    # Set paths to file check
+    clear_path = path_expand_vars_user
+    rel_wd_path = os.path.join(wd_path, path_expand_vars_user)
+    rel_script_path = os.path.join(script_dir_path, path_expand_vars_user)
+    real_rel_script_path = os.path.realpath(rel_script_path)
+    # Check on file:
+    result = dict()
+    if os.path.isfile(clear_path):
+        result['type'] = 0
+        result['path'] = clear_path
+    elif os.path.isfile(rel_wd_path):
+        result['type'] = 1
+        result['path'] = rel_wd_path
+    elif os.path.isfile(rel_script_path):
+        result['type'] = 2
+        result['path'] = rel_script_path
+    elif os.path.isfile(real_rel_script_path):
+        result['type'] = 3
+        result['path'] = real_rel_script_path
+    else:  # No file
+        result['type'] = -1
+        result['path'] = path
+    return result
+
+
 def main():
-    print('Arguments')
+    print('Python: {0}'.format(sys.executable))
+    print('Script: {0}'.format(__file__))
+    print('Working Directory: {0}'.format(os.getcwd()))
+    print('Command Line Arguments:')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help='path to config json file')
+    parser.add_argument('-c', '--config', help='path to config json file', default='process_messenger_config.json')
     parser.add_argument('-i', '--input', help='path to input json file')
     parser.add_argument('-p', '--pids', help='processes ids', nargs='+')
     parser.add_argument('-l', '--logs', help='processes logs paths', nargs='+')
     parser.add_argument('-n', '--names', help='processes names', nargs='+')
     args = parser.parse_args()
     print(args)
-    if args.config is None:
-        dirname = os.path.dirname(os.path.realpath(__file__))
-        config_path = os.path.join(dirname, 'process_messenger_config.json')
-    else:
-        print('-c key presented - override config path')
-        config_path = args.config
-    print('Config file: {0}'.format(config_path))
-    # Get config
-    # "input": "path to input json file",
-    # "ssh": "username@host" or "host" or null,
-    # "mailing_list": ["local-part@domain", ...],
-    # "n_log_lines": int,
-    # "full_log": bool
+    # Config
+    result = check_file(args.config)
+    config_path = result['path']
+    print('Config: {0}'.format(config_path))
     with open(config_path) as f:
         config_args = json.load(f)
-    print('Config')
     pprint(config_args)
-    # Get input from input file or from command line
-    # "pid": {
-    #   "abs_log_path": "abs path to process log file" or null,
-    #   "status": 1 or 0 or null,
-    #   "name": "process name" or null
-    # }
+    # Input
     if args.input is None:
-        input_file_path = os.path.expanduser(config_args['input'])
-    else:  # if -i key
-        print('-i key presented - override config input')
-        input_file_path = os.path.expanduser(args.input)
-    print('Input file: {0}'.format(input_file_path))
+        result = check_file(config_args['input'])
+        input_path = result['path']
+    else:
+        print('-i key presented -> override config input')
+        result = check_file(args.input)
+        input_path = result['path']
+    print('Input: {0}'.format(input_path))
     if args.pids is None:
-        with open(input_file_path) as f:
+        with open(input_path) as f:
             processes = json.load(f)
-    else:  # if -p key
-        print('-p key presented - override input processes')
+        # Check log_paths:
+        for i, pid in enumerate(processes):
+            log_path = processes[pid].get('log_path', None)
+            if log_path is not None:
+                result = check_file(log_path)
+                if result['type'] >= 0:
+                    processes[pid]['log_path'] = result['path']
+    else:
+        print('-p key presented -> override input pids')
         processes = dict()
         for i, pid in enumerate(args.pids):
             processes[pid] = dict()
-            if args.logs is not None:  # if -l key
-                print('-l key presented - override input processes logs')
-                abs_log_path = os.path.expanduser(args.logs[i]) if i < len(args.logs) else None
-                processes[pid]['abs_log_path'] = abs_log_path
-            if args.names is not None:  # if -n key
-                print('-n key presented - override input processes names')
+            if args.logs is not None:
+                print('-l key presented -> override input pids logs')
+                log_path = args.logs[i] if i < len(args.logs) else None
+                if log_path is not None:
+                    result = check_file(log_path)
+                    if result['type'] >= 0:
+                        processes[pid]['log_path'] = result['path']
+            if args.names is not None:
+                print('-n key presented -> override input pids names')
                 name = args.names[i] if i < len(args.names) else None
                 processes[pid]['name'] = name
-    print('Processes')
     pprint(processes)
     print('Initializing ProcessMessenger')
     pm = ProcessMessenger(
@@ -175,7 +214,7 @@ def main():
         is_changed = pm.check_processes()  # Update processes statuses
         if is_changed:
             print('Updating input file')
-            with open(input_file_path, 'w') as f:
+            with open(input_path, 'w') as f:
                 json.dump(pm.processes, f, indent=2)
             print('Done updating input file')
         all_done = True
